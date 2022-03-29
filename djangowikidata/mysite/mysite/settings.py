27c16ced8869959d5f75c394948de5245aeb220d
Berat Postalcioglu <bpstlcgl@gmail.com>
2022-03-29 19:10:07 +0300

    add elearning requirements

--- djangowikidata/mysite/mysite/settings.py
+++ djangowikidata/mysite/mysite/settings.py
@@ -20,6 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("DJANGO_SECRET")
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -72,7 +75,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-  'default': {
+    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'djangowikidata',
         'USER': os.environ.get("DB_USER"),
---
+++ elearning/Requirements.md
@@ -0,0 +1,11 @@
+
+* Users shall be able to create a profile with an email and a password.
+* Users shall be able to search a keyword.
+* The system shall list relevant courses for a searched keyword.
+* Users shall be able to inspect main page of a course.
+* Users shall be able to watch introduction of a course.
+* Users shall be able to enrol to a course.
+* If a course that a user wants to enrol requires payment, then the system shall enable the user to use his/her credit card information for the course payment.
+* Users shall be able to see their enrolled courses.
+* Users shall be able to see all of the content of their enrolled courses.
+
