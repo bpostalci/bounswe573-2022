03715611a5de29acc24e4eed2e2c6bf4ecc7d64b
Berat Postalcioglu <bpstlcgl@gmail.com>
2022-05-22 19:15:35 +0300

    - Create model Course - Create model Topic - Add field bio to user - Add field create_date to user - Alter field email on user - Alter field password on user - Create model Question - Add field topics to course - Create model Answer - Add field courses to user - Add field topics to user

--- edume/backend/edume/app/models.py
+++ edume/backend/edume/app/models.py
@@ -2,15 +2,62 @@ from django.db import models
 from django.utils import timezone
 
 
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+    create_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    instructor_name = models.CharField(max_length=200)
+    syllabus = models.CharField(max_length=1000)
+    create_date = models.DateTimeField(default=timezone.now)
+    topics = models.ManyToManyField(Topic)
+
+    def publish(self):
+        self.save()
+
+
 class User(models.Model):
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
     email = models.EmailField(max_length=50)
     birth_date = models.DateTimeField(default=timezone.now)
     password = models.CharField(max_length=200)
+    create_date = models.DateTimeField(default=timezone.now)
+    topics = models.ManyToManyField(Topic)
+    courses = models.ManyToManyField(Course)
+    bio = models.CharField(max_length=500, default='')
 
     def publish(self):
         self.save()
 
     def __str__(self):
         return self.name + ' ' + self.surname
+
+
+class Question(models.Model):
+    asked_user = models.CharField(max_length=400)
+    question = models.CharField(max_length=300)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.save()
+
+
+class Answer(models.Model):
+    answered_user = models.CharField(max_length=400)
+    answer = models.CharField(max_length=300)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.save()
