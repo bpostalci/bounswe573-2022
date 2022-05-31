6f993aba554cb62ff4833e7d1bbad7eac761af67
Berat Postalcioglu <bpstlcgl@gmail.com>
2022-05-31 15:57:53 +0300

    add test cases

--- edume/backend/edume/app/tests.py
+++ edume/backend/edume/app/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+from edume.app.models import User
+
+
+def test_user_creation():
+    user = User.objects.create(name="Berat",
+                               surname="Postalcioglu",
+                               email="bpostalci@gmail.com",
+                               password="123",
+                               bio="")
+    assert user is not None
